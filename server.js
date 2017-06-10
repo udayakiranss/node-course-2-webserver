75ae38fef0d3b2ff63724b51831e777a16ec18a3
udayakiranss <udaykiranss@icloud.com>
2017-06-11 00:53:28 +0530

    Added new link to Projects

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about',(req,res)=>{
     });
 });
 
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle:"Project Page",
+        projectLink: "https://github.com/udayakiranss/node-course-2-webserver"
+    });
+});
+
 app.get('/bad',(req,res)=>{
     res.send({
         errorMessage:"Problem in rendering the page"
